7eebf923ed89851f96dd98e4fd8f9cfd92564cf9
Ken <playerken@163.com>
2018-03-03 18:41:01 +0800

    init junit

---
+++ src/main/java/tmp/Hello.java
@@ -0,0 +1,10 @@
+package tmp;
+
+/**
+ * Created by playe on 2018/3/3.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/tmp/HelloTest.java
@@ -0,0 +1,14 @@
+package tmp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by playe on 2018/3/3.
+ */
+public class HelloTest {
+    @Test
+    public void testHello() {
+        Assert.assertEquals(1, 5 / 5 );
+    }
+}
